16694e0e0ba665305a47e6b23df8c32d54b7b15a
Daniel Robson <danielijrobson@gmail.com>
2017-11-26 23:50:07 +0000

    .

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 # include <errno.h>
 # include <string.h>
 # include <sys/types.h>
-
+# include <stdlib.h>
 char const tmpf_tmpl[] = "/tmp/rbc.XXXXXX";
 int main(int __argc, char const *__argv[]) {
 	char const *pth = *__argv;
@@ -30,10 +30,15 @@ int main(int __argc, char const *__argv[]) {
 	fread(raw_bc, 1, tf_size, fp);
 	write(tf_d, raw_bc, tf_size);
 
+	pid_t pid = fork();
+
+	if (pid == 0) {
 	char *argv[] = {"bci", tmpf_name, NULL};
-	if (execvp("bci", argv) < 0) {
-		fprintf(stderr, "failed, errno: %d\n", errno);
+		if (execvp("bci", argv) < 0) {
+			fprintf(stderr, "failed, errno: %d\n", errno);
+		}
 	}
+	waitpid(pid, NULL, 0);
 
 	unlink(tmpf_name);
 	fclose(fp);
